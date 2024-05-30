478e388e28d0b7d0a03d0cb3b075cf303959213f
Andrii Valenko <andreyvalenko@gmail.com>
2024-05-30 12:47:58 +0300

    feat: add base config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { BaseModule } from './modules';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), BaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      models: [],
+      synchronize: true,
+    }),
+    BaseModule,
+  ],
 })
 export class AppModule {}
