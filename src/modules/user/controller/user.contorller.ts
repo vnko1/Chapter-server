c65212c4e3d69b7957ddb065bc045453eaf71022
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-06-02 14:40:02 +0300

    add delete user account

--- src/modules/user/controller/user.contorller.ts
+++ src/modules/user/controller/user.contorller.ts
@@ -1,13 +1,18 @@
-import { Controller, Delete, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
 
+import { CredEnum } from 'src/types';
+import { AppService } from 'src/common/services';
 import { Public, UserData } from 'src/common/decorators';
 
 import { User } from '../model';
 import { UserService } from '../service';
 
 @Controller('user')
-export class UserController {
-  constructor(private userService: UserService) {}
+export class UserController extends AppService {
+  constructor(private userService: UserService) {
+    super();
+  }
 
   @Get()
   async getMe(@UserData() user: User) {
@@ -15,8 +20,12 @@ export class UserController {
   }
 
   @Delete()
-  async deleteUser(@UserData('id') id: string) {
-    return id;
+  async deleteUser(@UserData('id') id: string, @Res() res: Response) {
+    await this.userService.updateUser(
+      { accountStatus: 'deleted' },
+      { where: { id } },
+    );
+    return this.deleteCookieResponse(res, CredEnum.Refresh_token).send();
   }
 
   @Public()
