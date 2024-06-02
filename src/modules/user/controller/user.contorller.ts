ca7fff2c71ff092b6aa2be6aeb53d488671f1a4e
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-06-02 14:09:11 +0300

    add temp controller

--- src/modules/user/controller/user.contorller.ts
+++ src/modules/user/controller/user.contorller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+
 import { UserData } from 'src/common/decorators';
+
 import { User } from '../model';
+import { UserService } from '../service';
 
 @Controller('user')
 export class UserController {
-  constructor() {}
+  constructor(private userService: UserService) {}
 
   @Get()
   async getMe(@UserData() user: User) {
     return user;
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    return await this.userService.findUserByPK(id);
   }
 }
