104a78eaf053ea7f520d8a871210c7e885c7b41d
Andrii Valenko <andreyvalenko@gmail.com>
2024-06-05 18:02:17 +0300

    save

--- src/modules/posts/service/posts.service.ts
+++ src/modules/posts/service/posts.service.ts
@@ -2,10 +2,30 @@ import { Injectable } from '@nestjs/common';
 
 import { AppService } from 'src/common/services';
 import { PostService } from 'src/modules/post/service';
+import { CreatePostDto } from '../dto';
+import { CloudsService } from 'src/modules/clouds/service';
+import { UploadApiOptions } from 'cloudinary';
 
 @Injectable()
 export class PostsService extends AppService {
-  constructor(private postService: PostService) {
+  constructor(
+    private postService: PostService,
+    private cloudsService: CloudsService,
+  ) {
     super();
+  }
+
+  private uploadOption: UploadApiOptions = {
+    resource_type: 'image',
+    folder: 'chapter/avatar',
+    overwrite: true,
+  };
+
+  async addPost(createPostDto: CreatePostDto, id: string) {
+    const { image, ...postData } = createPostDto;
+
+    const post: CreatePostDto = { ...postData };
+
+    //   if(image)
   }
 }
