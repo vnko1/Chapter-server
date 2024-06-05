d9fd7997f3b4ddea586ba425a6313743559a18cb
Andrii Valenko <andreyvalenko@gmail.com>
2024-06-05 17:57:43 +0300

    add add post route

--- src/modules/posts/controller/posts.controller.ts
+++ src/modules/posts/controller/posts.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
+import { multerConfig } from 'src/utils';
+import { UserData } from 'src/common/decorators';
+
 import { PostsService } from '../service';
+import { CreatePostDto, createPostSchema } from '../dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private postsService: PostsService) {}
+
+  @Post('post')
+  @UseInterceptors(
+    FileInterceptor('image', { storage: diskStorage(multerConfig) }),
+  )
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async createPost(
+    @UserData('id') id: string,
+    @Body() createPostDto: CreatePostDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    const parsedSchema = createPostSchema.safeParse({
+      ...createPostDto,
+      image,
+    });
+
+    if (!parsedSchema.success)
+      throw new BadRequestException(parsedSchema.error.errors[0].message);
+
+    return parsedSchema.data;
+  }
 }
