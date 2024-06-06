26d2ee0eabad81e8ed972bf37bf220db6f6676a4
Andrii Valenko <andreyvalenko@gmail.com>
2024-06-06 16:28:31 +0300

    add likeModule

--- src/modules/like/like.module.ts
+++ src/modules/like/like.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+
 import { Like } from './model';
+import { LikeService } from './service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Like])],
-  providers: [],
-  exports: [],
+  providers: [LikeService],
+  exports: [LikeService],
 })
 export class LikeModule {}
--- src/modules/like/service/like.service.ts
+++ src/modules/like/service/like.service.ts
@@ -1,3 +1,26 @@
+import { InjectModel } from '@nestjs/sequelize';
+import { Injectable } from '@nestjs/common';
+
 import { AppService } from 'src/common/services';
 
-export class LikeService extends AppService {}
+import { Like } from '../model';
+import { FindAndCountOptions } from 'sequelize';
+
+@Injectable()
+export class LikeService extends AppService {
+  constructor(
+    @InjectModel(Like) private likeModel: typeof Like,
+    // private postService: PostService,
+    // private userService: UserService,
+  ) {
+    super();
+  }
+
+  async addLike(postId: string, userId: string) {
+    return this.likeModel.create({ userId, postId });
+  }
+
+  async getLikes(opt?: Omit<FindAndCountOptions<any>, 'group'>) {
+    return this.likeModel.findAndCountAll(opt);
+  }
+}
