fffb47d06cb0ef831e975b2ad0f5da083b3108d8
Andrii Valenko <andreyvalenko@gmail.com>
2024-06-06 17:50:25 +0300

    edit like service

--- src/modules/like/service/like.service.ts
+++ src/modules/like/service/like.service.ts
@@ -1,26 +1,39 @@
 import { InjectModel } from '@nestjs/sequelize';
 import { Injectable } from '@nestjs/common';
+import { DestroyOptions, FindAndCountOptions, FindOptions } from 'sequelize';
 
 import { AppService } from 'src/common/services';
 
 import { Like } from '../model';
-import { FindAndCountOptions } from 'sequelize';
 
 @Injectable()
 export class LikeService extends AppService {
-  constructor(
-    @InjectModel(Like) private likeModel: typeof Like,
-    // private postService: PostService,
-    // private userService: UserService,
-  ) {
+  constructor(@InjectModel(Like) private likeModel: typeof Like) {
     super();
   }
 
   async addLike(postId: string, userId: string) {
-    return this.likeModel.create({ userId, postId });
+    return this.likeModel.create({ postId, userId });
   }
 
-  async getLikes(opt?: Omit<FindAndCountOptions<any>, 'group'>) {
+  async findLike(opt?: FindOptions) {
+    return this.likeModel.findOne(opt);
+  }
+
+  async findLikes(opt?: FindOptions) {
+    return this.likeModel.findAll(opt);
+  }
+
+  async findAndCountLikes(opt?: Omit<FindAndCountOptions<any>, 'group'>) {
     return this.likeModel.findAndCountAll(opt);
   }
+
+  async deleteLike(opt?: DestroyOptions) {
+    return this.likeModel.destroy(opt);
+  }
 }
+// async toggleLike(postId: string, userId: string) {
+//   const like = await this.likeModel.findOne({ where: { postId, userId } });
+//   if (like) await like.destroy();
+//   else await this.likeModel.create({ postId, userId });
+// }
