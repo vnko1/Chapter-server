81428e7676720d28324cdb8dc80b1c863f359f88
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-06-07 20:16:11 +0300

    add get comments route

--- src/modules/comments/controller/comments.controller.ts
+++ src/modules/comments/controller/comments.controller.ts
@@ -1,21 +1,26 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Delete,
+  Get,
   HttpCode,
   HttpStatus,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 
+import { LIMIT } from 'src/utils';
+import { DataGuard } from 'src/common/guards';
 import { UserData } from 'src/common/decorators';
 import { ZodValidationPipe } from 'src/common/pipes';
 
 import { CommentsService } from '../service';
 import { CommentDto, commentSchema } from '../dto';
-import { DataGuard } from 'src/common/guards';
 
 @Controller('comments')
 export class CommentsController {
@@ -55,4 +60,11 @@ export class CommentsController {
   async deleteComment(@Param('commentId') commentId: string) {
     return await this.commentsService.deleteComment(commentId);
   }
+
+  @Get(':postId')
+  async getComments(
+    @Param('postId') commentId: string,
+    @Query('limit', new DefaultValuePipe(LIMIT), ParseIntPipe) limit: number,
+    @Query('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number,
+  ) {}
 }
