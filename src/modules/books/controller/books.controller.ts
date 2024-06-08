4a21316202d22227ff128d9ee1abc641d146c890
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-06-08 14:34:22 +0300

    save

--- src/modules/books/controller/books.controller.ts
+++ src/modules/books/controller/books.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  @Get() getBooks() {
+    return 1;
+  }
+}
