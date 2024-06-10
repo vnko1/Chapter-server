0a5fa83cf989cfb5496a81668234c73341b4cb03
Andrii Valenko <andreyvalenko@gmail.com>
2024-06-10 14:02:53 +0300

    add search module

--- src/modules/search/controller/search.controller.ts
+++ src/modules/search/controller/search.controller.ts
@@ -1,4 +1,14 @@
-import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+
+import { LIMIT } from 'src/utils';
+
 import { SearchService } from '../service';
 import { searchSchema } from '../dto';
 
@@ -7,12 +17,20 @@ export class SearchController {
   constructor(private searchService: SearchService) {}
 
   @Get()
-  async searchData(@Query('query') query: string) {
+  async searchData(
+    @Query('query') query: string,
+    @Query('limit', new DefaultValuePipe(LIMIT), ParseIntPipe) limit: number,
+    @Query('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number,
+  ) {
     const parsedSchema = searchSchema.safeParse(query);
 
     if (!parsedSchema.success)
       throw new BadRequestException(parsedSchema.error.errors[0].message);
 
-    return await this.searchService.searchData(parsedSchema.data);
+    return await this.searchService.searchData(
+      parsedSchema.data,
+      offset,
+      limit,
+    );
   }
 }
--- src/modules/search/service/search.service.ts
+++ src/modules/search/service/search.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Op } from 'sequelize';
-import { Sequelize } from 'sequelize-typescript';
 
 import { AppService } from 'src/common/services';
 import { BookService } from 'src/modules/book';
 import { PostService } from 'src/modules/post';
-
 import { UserService } from 'src/modules/user';
 
 @Injectable()
 export class SearchService extends AppService {
   constructor(
-    private sequelize: Sequelize,
     private userService: UserService,
     private postService: PostService,
     private bookService: BookService,
@@ -19,8 +16,18 @@ export class SearchService extends AppService {
     super();
   }
 
-  async searchData(query: string) {
-    const users = await this.userService.getAllUsers(
+  async searchData(query: string, offset: number, limit: number) {
+    const [users, posts, books] = await Promise.all([
+      this.userSearch(query, offset, limit),
+      this.postsSearch(query, offset, limit),
+      this.booksSearch(query, offset, limit),
+    ]);
+
+    return { users, posts, books };
+  }
+
+  async userSearch(query: string, offset: number, limit: number) {
+    return await this.userService.findAndCountData(
       {
         where: {
           [Op.or]: [
@@ -32,10 +39,38 @@ export class SearchService extends AppService {
             { status: { [Op.like]: `%${query}%` } },
           ],
         },
+        offset,
+        limit,
       },
       'publicScope',
     );
+  }
 
-    return users;
+  async postsSearch(query: string, offset: number, limit: number) {
+    return await this.postService.findAndCountPosts({
+      where: {
+        [Op.or]: [
+          { title: { [Op.like]: `%${query}%` } },
+          { text: { [Op.like]: `%${query}%` } },
+        ],
+      },
+      offset,
+      limit,
+    });
+  }
+
+  async booksSearch(query: string, offset: number, limit: number) {
+    return await this.bookService.findAndCountBooks({
+      where: {
+        [Op.or]: [
+          { bookName: { [Op.like]: `%${query}%` } },
+          { author: { [Op.like]: `%${query}%` } },
+          { annotation: { [Op.like]: `%${query}%` } },
+          { bookStatus: { [Op.like]: `%${query}%` } },
+        ],
+      },
+      offset,
+      limit,
+    });
   }
 }
