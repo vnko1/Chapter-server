51e2cff3a59341e6246f17d7e088ea4b0a63e583
Andrii Valenko <andreyvalenko@gmail.com>
2024-06-11 17:50:41 +0300

    save updates

--- src/modules/socket/services/socket.service.ts
+++ src/modules/socket/services/socket.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { WsException } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
 @Injectable()
 export class SocketService {
   private readonly connectedClients: Map<string, Socket> = new Map();
 
+  constructor(private jwtService: JwtService) {}
+
   handleConnection(socket: Socket): void {
-    console.log(
-      '🚀 ~ SocketService ~ handleConnection ~ socket:',
-      socket.handshake.headers.token,
-    );
-    const clientId = socket.id;
-    this.connectedClients.set(clientId, socket);
-    socket.on('disconnect', () => {
-      this.connectedClients.delete(clientId);
-    });
+    try {
+      const token = socket.handshake.headers.token;
+      if (!token) throw new WsException('Unauthorized');
+
+      const clientId = socket.id;
+      this.connectedClients.set(clientId, socket);
+      socket.on('disconnect', () => {
+        this.connectedClients.delete(clientId);
+      });
+    } catch (error) {
+      socket.disconnect();
+    }
   }
 
   emitEvent(event: string, data: any): void {
