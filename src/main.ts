45d2426f3eced1f7017a0e7523f02af74945be85
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-06-22 06:22:26 +0300

    save upd

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://chapter-client.vercel.app'],
+    origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
     credentials: true,
     allowedHeaders: [
